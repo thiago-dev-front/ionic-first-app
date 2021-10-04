05a8abacde66351f8b60a8edfa63213c190bed09
Thiago Diógenes <thiago.maia@dotzmkt.com.br>
2021-10-03 22:19:18 -0300

    Criação Login + Estrutura + Forms

---
+++ src/app/auth/pages/login/login.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  authForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  onSubmit(): void {
+    console.log('AuthForm: ', this.authForm.value);
+  }
+
+  private createForm(): void {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  get email(): FormControl {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    return <FormControl>this.authForm.get('email');
+  }
+
+  get password(): FormControl {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    return <FormControl>this.authForm.get('password');
+  }
+}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+@NgModule({
+  imports: [IonicModule.forRoot()],
+  exports: [BrowserModule, IonicModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+})
+export class CoreModule {}
