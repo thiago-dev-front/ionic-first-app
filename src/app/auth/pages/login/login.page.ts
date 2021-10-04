16842b29214e4de27c2d5556da9c2de58f9a6fc0
Thiago Diógenes <thiago.maia@dotzmkt.com.br>
2021-10-04 19:31:15 -0300

    Already Account + Login

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -13,11 +13,35 @@ import {
 })
 export class LoginPage implements OnInit {
   authForm: FormGroup;
+  configs = {
+    isSignIn: true,
+    action: 'Login',
+    actionChange: 'Create account',
+  };
+
+  private nameControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.createForm();
+  }
+
+  // Alternância de método na página
+  changeAuthAction(): void {
+    this.configs.isSignIn = !this.configs.isSignIn;
+    const { isSignIn } = this.configs;
+    this.configs.action = isSignIn ? 'Login' : 'Sign Up';
+    this.configs.actionChange = isSignIn
+      ? 'Create account'
+      : 'Already have an account';
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    !isSignIn
+      ? this.authForm.addControl('name', this.nameControl)
+      : this.authForm.removeControl('name');
   }
 
   onSubmit(): void {
@@ -29,6 +53,11 @@ export class LoginPage implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
+  }
+
+  get name(): FormControl {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    return <FormControl>this.authForm.get('name');
   }
 
   get email(): FormControl {
